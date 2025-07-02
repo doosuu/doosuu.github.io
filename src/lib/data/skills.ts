85673ab5770da354d286eaec83d9c9605f4ecd71
Dominic Sudy <dominic.sudy@gmail.com>
2025-07-02 22:42:36 +0200

    feat: Skill graph

---
+++ src/lib/data/skills.ts
@@ -0,0 +1,176 @@
+import type { Data } from "$lib/types";
+
+export const skills: Data = {
+    "name": "me",
+    "children": [
+        {
+            "name": "coding",
+            "children": [
+                {
+                    "name": "c++",
+                    "level": "expert"
+                },
+                {
+                    "name": "TypeScript",
+                    "level": "advanced"
+                },
+                {
+                    "name": "Python",
+                    "level": "expert"
+                },
+                {
+                    "name": "Java",
+                    "level": "intermediate"
+                },
+                {
+                    "name": "C#",
+                    "level": "intermediate"
+                },
+                {
+                    "name": "Rust",
+                    "level": "beginner"
+                }
+            ]
+        },
+        {
+            "name": "best practices",
+            "children": [
+                {
+                    "name": "clean code"
+                },
+                {
+                    "name": "test driven development"
+                },
+                {
+                    "name": "mentoring"
+                },
+            ]
+        },
+        {
+            "name": "languages",
+            "children": [
+                {
+                    "name": "English",
+                    "level": "advanced"
+                },
+                {
+                    "name": "German",
+                    "level": "expert"
+                }
+            ]
+        },
+        {
+            "name": "methods",
+            "children": [
+                {
+                    "name": "Agile",
+                    "level": "advanced"
+                },
+                {
+                    "name": "Scrum",
+                    "level": "advanced"
+                },
+                {
+                    "name": "Kanban",
+                    "level": "intermediate"
+                },
+                {
+                    "name": "Waterfall",
+                    "level": "advanced"
+                }
+            ]
+        },
+        {
+            "name": "os",
+            "children": [
+                {
+                    "name": "Linux",
+                    "level": "advanced"
+                },
+                {
+                    "name": "Windows",
+                    "level": "advanced"
+                },
+                {
+                    "name": "macOS",
+                    "level": "advanced"
+                },
+                {
+                    "name": "Android",
+                    "level": "intermediate"
+                },
+                {
+                    "name": "QNX",
+                    "level": "beginner"
+                }
+            ]
+        },
+        {
+            "name": "mindset",
+            "children": [
+                {
+                    "name": "problem solving",
+                    "level": "expert"
+                },
+                {
+                    "name": "teamwork",
+                    "level": "expert"
+                },
+                {
+                    "name": "communication",
+                    "level": "advanced"
+                },
+                {
+                    "name": "adaptability",
+                    "level": "advanced"
+                },
+                {
+                    "name": "creativity",
+                    "level": "intermediate"
+                },
+                {
+                    "name": "leading by example",
+                    "level": "advanced"
+                },
+                {
+                    "name": "mentoring",
+                    "level": "advanced"
+                }
+            ]
+        },
+        {
+            "name": "roles",
+            "children": [
+                {
+                    "name": "(Senior) SW Engineer",
+                    "level": "expert",
+                },
+                {
+                    "name": "SW Architect",
+                    "level": "advanced",
+                },
+                {
+                    "name": "Technical lead",
+                    "level": "advanced",
+                }
+            ]
+        },
+        {
+            "name": "domain knowledge",
+            "children": [
+                {
+                    "name": "automotive",
+                    "level": "expert"
+                },
+                {
+                    "name": "cloud",
+                    "level": "intermediate"
+                },
+                {
+                    "name": "3D graphics",
+                    "level": "expert"
+                }
+            ]
+        }
+    ]
+}
